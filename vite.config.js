3173ed88bd7fee4ac425e22a9a9052753abc3caa
Mohamed Ali Jarray <132663915+dalijarray@users.noreply.github.com>
2024-11-09 17:38:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'; // eslint-disable-line
 import react from '@vitejs/plugin-react'; // eslint-disable-line
 
 export default defineConfig({
-  base: '/',
+  base: '/Profile/',
   plugins: [react()],
   build: {
     rollupOptions: {
