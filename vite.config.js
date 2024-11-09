10dab79f0d6271379faa63cefaa90c8c64364c64
Dali <mohamedalijarray870@gmail.com>
2024-11-09 20:11:19 +0100

    hgdk

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'; // eslint-disable-line
 import react from '@vitejs/plugin-react'; // eslint-disable-line
 
 export default defineConfig({
-  base: '/Profile/',
+  base: '/Profile11/',
   plugins: [react()],
   build: {
     rollupOptions: {
